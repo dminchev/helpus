b0bf941b1ee0314cfd3a363a90a4c003c0e90d8a
Dobromir Minchev <dobri87@gmail.com>
2011-01-24 17:52:47 +0200

    set active magazine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   def get_magazine
-    @magazine = Magazine.last
+    if session[:magazine_id] == nil
+      @magazine = Magazine.where("active = ?", 1).first
+    else
+      @magazine = Magazine.find(session[:magazine_id])
+    end
+
+    if params[:magazine_id]
+      @magazine = Magazine.find(params[:magazine_id])
+      session[:magazine_id] = params[:magazine_id]
+    end
+
     @accents = @magazine.accents
   end
 end
