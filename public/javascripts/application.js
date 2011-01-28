d9fc7df40cf797b108ebfca1a54aa70b9d3e2493
Dobromir Minchev <dobri87@gmail.com>
2011-01-28 11:41:17 +0200

    google analytics

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -36,3 +36,13 @@ function roll(img_name, img_src)
 {
   document[img_name].src = img_src;
 }
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-21035414-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
