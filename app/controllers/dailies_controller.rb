3f32824a2a2ed3afd884635759f72a5c2f59eb52
somuchbetter <dobri87@gmail.com>
2011-02-01 08:39:21 +0200

    fixes

--- app/controllers/dailies_controller.rb
+++ app/controllers/dailies_controller.rb
@@ -1,14 +1,14 @@
-class DailiesController < ApplicationController 
+class DailiesController < ApplicationController
   before_filter :get_magazine
   
   def index
-    @model = @daily = @magazine.dailies.first
     @dailies_list = @magazine.dailies.search(params[:page], 3)
+    @model = @daily = @dailies_list.first
   end
   
   def show
-    @model = @daily = Daily.find(params[:id])
     @dailies_list = @magazine.dailies.search(params[:page], 3)
+    @model = @daily = Daily.find(params[:id])
   end
   
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,14 +11,14 @@ function fixContent()
     var popupPositionLeft = (pageWidth-960)/2;
     $(".white_content").attr("style", "left: " + popupPositionLeft + "px;");
     $("#fade.black_overlay").height(pageHeight);
-    //$(".left_panel_content").height( newheight-96 + "px");                
+    //$(".left_panel_content").height( newheight-96 + "px");
   }
 }
 
 function fixCover()
 {
   $("#cover-image").height("0px");
-  var height = $(window).height();  
+  var height = $(window).height();
   $("#cover-image").height(height-20 + "px");
 }
 
@@ -28,7 +28,7 @@ function setGalleryArrows()
   var nextPageHref = $(".next_page").attr("href");
   
   $(".arrow-left").attr("href", prevPageHref);
-  $(".arrow-right").attr("href", nextPageHref);  
+  $(".arrow-right").attr("href", nextPageHref);
   $(".pagination").attr("style", "display:none;");
 }
 
@@ -36,6 +36,14 @@ function roll(img_name, img_src)
 {
   document[img_name].src = img_src;
 }
+
+$(document).ready(function () {
+	$("ul.menu_body li:even").addClass("alt");
+  $('img.menu_head').click(function () {
+	  $('ul.menu_body').slideToggle('medium');
+  });
+});
+    
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-21035414-1']);
@@ -46,3 +54,5 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+
