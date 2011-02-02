5e84a8e44b8db89091979f196e102cd27ace1a3b
Dobromir Minchev <dobri87@gmail.com>
2011-02-02 18:37:35 +0200

    order + 2 mgazine fixes

--- app/controllers/dailies_controller.rb
+++ app/controllers/dailies_controller.rb
@@ -1,14 +1,14 @@
-class DailiesController < ApplicationController 
+class DailiesController < ApplicationController
   before_filter :get_magazine
-  
+
   def index
     @model = @daily = @magazine.dailies.first
-    @dailies_list = @magazine.dailies.search(params[:page], 3)
+    @dailies_list = @magazine.dailies.order("`order` ASC").search(params[:page], 3)
   end
-  
+
   def show
     @model = @daily = Daily.find(params[:id])
-    @dailies_list = @magazine.dailies.search(params[:page], 3)
+    @dailies_list = @magazine.dailies.order("`order` ASC").search(params[:page], 3)
   end
-  
+
 end
